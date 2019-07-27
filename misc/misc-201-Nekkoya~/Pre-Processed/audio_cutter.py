8bb7df02a0ce4b49768bceda864c583da273b554
Dwakez <dwakez250@gmail.com>
2019-07-27 23:34:39 +0700

    remove all pre-private problems

--- misc/misc-201-Nekkoya~/Pre-Processed/audio_cutter.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import librosa
-
-signal, sr = librosa.load('./00. uncut.wav')
-signal = signal[int(65 * sr):int(65 * sr) + int(3.5 * sr)]
-
-librosa.output.write_wave('./01. pre.wav')
